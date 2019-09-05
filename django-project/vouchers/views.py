c5dbc439d417a21e44f6ab7a8febc872d4cb23c7
Faezeh Amri <amrifaezeh@gmail.com>
2019-09-05 19:23:52 +0800

    added a simple view

--- django-project/vouchers/views.py
+++ django-project/vouchers/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import Voucher
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('home/index.html')
+    return HttpResponse(template.render(None, request))
 
 # Create your views here.
