d4bba853f87b4c15bbbd09b02972ce0945ebe549
Faezeh Amri <amrifaezeh@gmail.com>
2019-09-06 14:09:57 +0800

    added alert and success page

--- django-project/vouchers/urls.py
+++ django-project/vouchers/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'vouchers'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('consume', views.consume, name='consume'),
+    path('success/<str:voucher_code>', views.success, name='success'),
+    path('error', views.error, name='error')
 ]
--- django-project/vouchers/views.py
+++ django-project/vouchers/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import F
 from django.template import loader
-from django.http import HttpResponse
 from .models import Voucher
+
+def get_context(message='', error_message=''):
+    available_vouchers= Voucher.objects.filter(usage_number__lt=F('max_usage'))
+    context={
+        'available_vouchers': available_vouchers,
+        'message': message,
+        'error_message': error_message,
+    }
+
+    return context
+    
+def success(request, voucher_code):
+    print(voucher_code)
+    code= get_object_or_404(Voucher, voucher_code= voucher_code)
+    context= get_context(f'congratulation you got {code.discount_value}{code.discount_type} off on your purchase')
+    return render(request, 'home/index.html', context)
+
+def error(request):
+    context= get_context('',"this voucher exceed the allowed usage number!")
+    return render(request, 'home/index.html', context)
 
 def index(request):
     template = loader.get_template('home/index.html')
-    return HttpResponse(template.render(None, request))
+    
+    return render(request, 'home/index.html', get_context())
+
+def consume(request):
+    voucher_code= request.POST['voucher_code']
+    
+    code= get_object_or_404(Voucher, voucher_code= voucher_code)
+
+    if (code.usage_number >=code.max_usage ):
+        return redirect('/error')
+
+    code.usage_number+=1
+    code.save()
+
+    return redirect(f'/success/{code.voucher_code}')
 
 # Create your views here.
